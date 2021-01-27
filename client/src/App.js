3c950cc0afb9d3537699f610561eb43edc072925
Aadarsh <aadarsh-s@outlook.com>
2021-01-28 04:21:47 +0530

    Adding skaffold config

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import PostList from './PostList';
 
 export default () => {
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Create Post</h1>
       <PostCreate />
       <hr />
--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -7,7 +7,7 @@ export default ({ postId }) => {
   const onSubmit = async event => {
     event.preventDefault();
 
-    await axios.post(`http://posts.com/posts/${postId}/comments`, {
+    await axios.post(`http://posts:30787.com/posts/${postId}/comments`, {
       content,
     });
 
